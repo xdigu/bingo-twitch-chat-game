46e87d2b4a353cdf7971d52d4b3d6e0a19514315
Rodrigo Jorge <xdigu@live.com>
2021-02-10 00:28:13 -0300

    added DTO validation

--- src/userCase/CreateBingoGame/CreateBingoGameController.ts
+++ src/userCase/CreateBingoGame/CreateBingoGameController.ts
@@ -1,14 +1,22 @@
 import { Request, Response } from 'express'
-import { ICreateBingoGameRequestDTO } from './CreateBingoGameDTO'
+import { ICreateBingoGameRequestDTO, createBingoGameRequestDTOValidation } from './CreateBingoGameDTO'
 import { CreateBingoGameUseCase } from './CreateBingoGameUseCase'
 
 export class CreateBingoGameController {
   constructor(private createBingoGameUseCase: CreateBingoGameUseCase) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
-    const { streamerName, amountOfCards }: ICreateBingoGameRequestDTO = request.body
+    const body: ICreateBingoGameRequestDTO = request.body
 
     try {
+      const { value, error } = createBingoGameRequestDTOValidation.validate(body)
+
+      if (error) {
+        throw new Error(`The sended body is not correct. ${error.message}`)
+      }
+
+      const { streamerName, amountOfCards } = value
+
       const game = await this.createBingoGameUseCase.create(streamerName, amountOfCards)
 
       return response.status(201).json(game)
